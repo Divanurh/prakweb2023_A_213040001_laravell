5fc98bde5b3356fe2f04a054394af280336d7120
Divanurh <divanurhaffifah@gmail.com>
2023-11-12 22:26:48 +0700

    9. Database Seeder

--- coba-laravell/database/seeders/DatabaseSeeder.php
+++ coba-laravell/database/seeders/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace Database\Seeders;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -18,5 +21,52 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::create([
+            'name'=>'Moch Priamitra',
+            'email'=>'alif@gmail.com',
+            'password'=>bcrypt('12345')
+        ]);
+
+        User::create([
+            'name'=>'Diva Nur',
+            'email'=>'diva@gmail.com',
+            'password'=>bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' =>'Web Programing',
+            'slug' => 'web-programing'
+        ]);
+        Category::create([
+            'name' =>'Personal',
+            'slug' => 'personal'
+        ]);
+
+
+        Post::create([
+            'title'=> 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia reiciendis dolorum sint',
+            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia reiciendis dolorum sint voluptates totam ab magnam unde accusantium odio beatae cumque fugiat culpa vel, cupiditate dolore, consectetur alias veniam voluptate animi ea distinctio, quo rem quae nostrum! Ea et itaque quisquam eos autem neque iusto error repellendus accusamus dicta, dolorem aliquam modi quas asperiores facilis blanditiis ducimus dolore obcaecati quidem odio aliquid animi amet. Autem explicabo earum aperiam quas, cupiditate a fugiat! Eaque voluptatibus quo cum a dignissimos facere hic veniam laudantium. Distinctio ea minima maiores suscipit animi nam odio quisquam expedita, dicta nobis magnam iure repellat veniam esse unde eius nisi sunt necessitatibus error, deserunt modi provident nemo aspernatur. Porro possimus nisi voluptates quam nam amet aperiam labore explicabo.',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+        Post::create([
+            'title'=> 'Judul Keuda',
+            'slug' => 'judul-kedua',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia reiciendis dolorum sint',
+            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia reiciendis dolorum sint voluptates totam ab magnam unde accusantium odio beatae cumque fugiat culpa vel, cupiditate dolore, consectetur alias veniam voluptate animi ea distinctio, quo rem quae nostrum! Ea et itaque quisquam eos autem neque iusto error repellendus accusamus dicta, dolorem aliquam modi quas asperiores facilis blanditiis ducimus dolore obcaecati quidem odio aliquid animi amet. Autem explicabo earum aperiam quas, cupiditate a fugiat! Eaque voluptatibus quo cum a dignissimos facere hic veniam laudantium. Distinctio ea minima maiores suscipit animi nam odio quisquam expedita, dicta nobis magnam iure repellat veniam esse unde eius nisi sunt necessitatibus error, deserunt modi provident nemo aspernatur. Porro possimus nisi voluptates quam nam amet aperiam labore explicabo.',
+            'category_id' => 1,
+            'user_id' => 2
+        ]);
+        Post::create([
+            'title'=> 'Judul Ketiga',
+            'slug' => 'judul-ketiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia reiciendis dolorum sint',
+            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia reiciendis dolorum sint voluptates totam ab magnam unde accusantium odio beatae cumque fugiat culpa vel, cupiditate dolore, consectetur alias veniam voluptate animi ea distinctio, quo rem quae nostrum! Ea et itaque quisquam eos autem neque iusto error repellendus accusamus dicta, dolorem aliquam modi quas asperiores facilis blanditiis ducimus dolore obcaecati quidem odio aliquid animi amet. Autem explicabo earum aperiam quas, cupiditate a fugiat! Eaque voluptatibus quo cum a dignissimos facere hic veniam laudantium. Distinctio ea minima maiores suscipit animi nam odio quisquam expedita, dicta nobis magnam iure repellat veniam esse unde eius nisi sunt necessitatibus error, deserunt modi provident nemo aspernatur. Porro possimus nisi voluptates quam nam amet aperiam labore explicabo.',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
     }
 }
--- coba-laravell/resources/views/post.blade.php
+++ coba-laravell/resources/views/post.blade.php
@@ -4,11 +4,11 @@
     <article>
         <h1>{{ $post->title }}</h1>
 
-    <p>By. Priamitra in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+    <p>By. <a href="" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}"class="text-decoration-none">{{ $post->category->name }}</a></p>
 
         {!! $post->body !!}
     </article>
 
-    <a href="/blog">Back to Posts</a>
+    <a href="/blog" class="d-block mt-3">Back to Posts</a>
         
 @endsection
